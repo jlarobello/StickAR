77146cb16754c56d7481cebcfe95ce1cee7cefbf
Christopher Kim <jkim.kwan@gmail.com>
2018-03-10 13:24:15 -1000

    changed up the qr code generator screen

--- StickAR/StickAR/QRCodeGeneratorScreen.swift
+++ StickAR/StickAR/QRCodeGeneratorScreen.swift
@@ -9,59 +9,82 @@
 import UIKit
 
 class QRCodeGeneratorScreen: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-
+    
+//    @IBOutlet weak var btnCard: UIButton!
+    
+//    var isOpen = false
+//
+//    @IBAction func btnFlip(_ sender: Any) {
+//        if isOpen {
+//            isOpen = false
+//            let image = UIImage(named: "walle")
+//            btnCard.setImage(image, for: .normal)
+//            UIView.transition(with: btnCard, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+//        } else {
+//            isOpen = true
+//            let image = UIImage(named: "walle_qr_code")
+//            btnCard.setImage(image, for: .normal)
+//            UIView.transition(with: btnCard, duration: 0.3, options: .transitionFlipFromRight, animations: nil, completion: nil)
+//        }
+//    }
+//    @IBAction func importImage(_ sender: Any) {
+//
+//        let image = UIImagePickerController()
+//        image.delegate = self
+//
+//        image.sourceType = UIImagePickerControllerSourceType.photoLibrary
+//
+//        image.allowsEditing = false
+//
+//        self.present(image, animated: true) {
+//            // After it is complete
+//        }
+//    }
+    
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+//        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+//            myImageView.image = image
+//        } else {
+//            // Error Message
+//        }
+//
+//        self.dismiss(animated: true, completion: nil)
+//    }
+    
+    @IBOutlet weak var txtLink: UITextField!
+    @IBOutlet weak var generateBtn: UIButton!
     @IBOutlet weak var myImageView: UIImageView!
     
-    @IBAction func logoutHandler(_ sender: Any) {
-        presentingViewController?.dismiss(animated: true, completion: nil)
-    }
-    
-    @IBOutlet weak var btnCard: UIButton!
-    
-    var isOpen = false
-    
-    @IBAction func btnFlip(_ sender: Any) {
-        if isOpen {
-            isOpen = false
-            let image = UIImage(named: "walle")
-            btnCard.setImage(image, for: .normal)
-            UIView.transition(with: btnCard, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
-        } else {
-            isOpen = true
-            let image = UIImage(named: "walle_qr_code")
-            btnCard.setImage(image, for: .normal)
-            UIView.transition(with: btnCard, duration: 0.3, options: .transitionFlipFromRight, animations: nil, completion: nil)
-        }
-    }
-    @IBAction func importImage(_ sender: Any) {
-        
-        let image = UIImagePickerController()
-        image.delegate = self
-        
-        image.sourceType = UIImagePickerControllerSourceType.photoLibrary
-        
-        image.allowsEditing = false
-        
-        self.present(image, animated: true) {
-            // After it is complete
-        }
-    }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
-            myImageView.image = image
-        } else {
-            // Error Message
+    @IBAction func generateQRCode(_ sender: Any) {
+        func generateQRCode(from string: String) -> UIImage? {
+            let data = string.data(using: String.Encoding.ascii)
+            
+            if let filter = CIFilter(name: "CIQRCodeGenerator") {
+                filter.setValue(data, forKey: "inputMessage")
+                let transform = CGAffineTransform(scaleX: 3, y: 3)
+                
+                if let output = filter.outputImage?.transformed(by: transform) {
+                    return UIImage(ciImage: output)
+                }
+            }
+            
+            return nil
         }
         
-        self.dismiss(animated: true, completion: nil)
+        myImageView.image = generateQRCode(from: txtLink.text!)
     }
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        txtLink.layer.borderWidth = 1
+        txtLink.layer.borderColor = UIColor.white.cgColor
+        txtLink.layer.cornerRadius = 20
+        txtLink.clipsToBounds = true
+        
+        generateBtn.layer.cornerRadius = 20
+        generateBtn.clipsToBounds = true
+        
         // Do any additional setup after loading the view.
     }
 
