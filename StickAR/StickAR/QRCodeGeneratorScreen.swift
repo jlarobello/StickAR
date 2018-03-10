2c89c4f80195e9c83636862704d48f599f0a8bab
Christopher Kim <jkim.kwan@gmail.com>
2018-03-09 23:12:47 -1000

    swipe gestures, qr code gen screen, image upload
    added swipe gestures, qr code gen screen, image upload

---
+++ StickAR/StickAR/QRCodeGeneratorScreen.swift
@@ -0,0 +1,63 @@
+//
+//  QRCodeGeneratorScreen.swift
+//  StickAR
+//
+//  Created by Christopher Kim on 3/9/18.
+//  Copyright © 2018 Christopher Kim. All rights reserved.
+//
+
+import UIKit
+
+class QRCodeGeneratorScreen: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBAction func importImage(_ sender: Any) {
+        
+        let image = UIImagePickerController()
+        image.delegate = self
+        
+        image.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        image.allowsEditing = false
+        
+        self.present(image, animated: true) {
+            // After it is complete
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            myImageView.image = image
+        } else {
+            // Error Message
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
